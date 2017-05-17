76d7b76796585e8890e4c5dd00b967a619835fb9
Ian Martin <ian@imartin.net>
2017-05-17 11:31:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,17 @@
 import flexget, sys, os
 
 args = []
+debug = os.environ.get('FG_DEBUG',False)
+if debug in ['Yes','yes','true','True','on','1']:
+  debug = True
 if os.environ.get('FG_CONFIG'):
   args.extend(["-c", os.environ.get('FG_CONFIG')])
 if os.environ.get('FG_COMMAND'):
   args.extend(os.environ.get('FG_COMMAND','daemon start').split(' '))
 
 if len(args) > 0:
+  if debug:
+    print "Starting flexget with: %s" % args
   flexget.main(args)
 else:
   flexget.main()
