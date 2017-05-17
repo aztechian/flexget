948c476fc2b36d1d7fc104488e37d6b465378731
Ian Martin <ian@imartin.net>
2017-05-17 11:05:30 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
-import flexget
-import sys
+import flexget, sys, os
+args = []
+if os.environ.get('FG_CONFIG'):
+  args.extend(["-c", os.environ.get('FG_CONFIG')])
+if os.environ.get('FG_COMMAND'):
+  args.extend(os.environ.get('FG_COMMAND','daemon start').split(' '))
 
-flexget.main()
+if len(args) > 0:
+  flexget.main(args)
+else:
+  flexget.main()
